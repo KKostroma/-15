10c0c34a490abc98f35758a4ec8ce42000025bd1
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2022-01-13 17:01:17 +0300

    Финализация

--- Задание15/Program.cs
+++ Задание15/Program.cs
@@ -10,6 +10,87 @@ namespace Задание15
     {
         static void Main(string[] args)
         {
+            ArithProgression arithProgression = new ArithProgression();
+            arithProgression.setStart(1);
+            arithProgression.Const(2);
+            Console.WriteLine("Вывод первого числа:");
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine("Вывод второго числа:");
+            Console.WriteLine(arithProgression.getNext());
+            arithProgression.reset();
+            Console.WriteLine("Вывод числа после сброса:");
+            Console.WriteLine(arithProgression.getNext());
+            GeomProgression geomProgression = new GeomProgression();
+            geomProgression.setStart(5);
+            geomProgression.Const(2);
+            Console.WriteLine("Вывод первого числа:");
+            Console.WriteLine(geomProgression.getNext());
+            Console.WriteLine("Вывод второго числа:");
+            Console.WriteLine(geomProgression.getNext());
+            geomProgression.reset();
+            Console.WriteLine("Вывод числа после сброса:");
+            Console.WriteLine(geomProgression.getNext());
+            Console.ReadKey();
+        }
+        interface ISeries
+        {
+            void setStart(int x);
+            int getNext();
+            void reset();
+        }
+        class ArithProgression : ISeries
+        {
+            int X;
+            int Y;
+            int N;
+            public void setStart(int x)
+            {
+                X = x;
+                Y = X;
+                Console.WriteLine($"Установка значения арифметической прогрессии: {X}");
+            }
+            public int getNext()
+            {
+                Y += N;
+                return Y;
+            }
+
+            public void reset()
+            {
+                Y = X;
+            }
+            public void Const(int n)
+            {
+                N = n;
+                Console.WriteLine($"Общий член арифметической прогрессии : {N}");
+            }
+        }
+        class GeomProgression : ISeries
+        {
+            int X;
+            int Y;
+            int N;
+            public void setStart(int x)
+            {
+                X = x;
+                Y = X;
+                Console.WriteLine($"Установка значения геометрической прогрессии: {X}");
+            }
+            public int getNext()
+            {
+                Y *= N;
+                return Y;
+            }
+
+            public void reset()
+            {
+                Y = X;
+            }
+            public void Const(int n)
+            {
+                N = n;
+                Console.WriteLine($"Общий член геометрической прогрессии : {N}");
+            }
         }
     }
 }
